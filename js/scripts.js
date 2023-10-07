85f42823257b648c841233f44f23e87b422d9a3f
nurm04 <113950633+nurm04@users.noreply.github.com>
2023-10-08 00:23:44 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+// const settings = document.querySelector(".navbar .settings");
+// const kembali = document.querySelector(".login .kembali");
+// const login = document.querySelector(".login");
+// const register = document.querySelector(".register");
+// const Tlogin = document.querySelector(".Tlogin");
+// const Tregister = document.querySelector(".Tregister");
+
+// settings.addEventListener("click", function () {
+//     login.classList.add("ada");
+// });
+
+// kembali.addEventListener("click", function () {
+//     login.classList.remove("ada");
+// });
+
+// Tregister.addEventListener("click", function () {
+//     login.classList.remove("ada");
+//     register.classList.add("ada");
+// });
+
+// Tlogin.addEventListener("click", function () {
+//     register.classList.remove("ada");
+//     login.classList.add("ada");
+// });
